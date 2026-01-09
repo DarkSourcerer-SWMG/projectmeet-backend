f547397ba08a6b4dfc7889591ab07edda6e08d3d
DarkSourcerer-SWMG <158461898+DarkSourcerer-SWMG@users.noreply.github.com>
2026-01-09 18:54:04 +0100

    bomboclat

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
+            policy.WithOrigins("http://localhost:5173", "https://projectmeet-frontend.vercel.app")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
